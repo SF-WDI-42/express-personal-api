4126b2f5b34ca8959d4f04dc0d2703a2bee13d1a
AdamMenard <adammenard888@gmail.com>
2017-12-18 22:45:14 -0800

    just after index.html

--- seed.js
+++ seed.js
@@ -3,13 +3,24 @@
 
 var db = require('./models');
 
-var new_hobby = {description: "Sharp rocks. Middle of nowhere."}
-
-db.Hobby.create(new_hobby, function(err, hobby){
-  if (err){
-    return console.log("Error:", err);
-  }
-
-  console.log("Created new hobby", hobby._id)
-  process.exit(); // we're all done! Exit the program.
-})
+var projectsList = [{
+  projectsObj1KeyOne: 'keyOneValuePair',
+  projectsObj1keyTwo: 'keyOneValuePair',
+  projectsObj1keyThree: 'keyOneValuePair',
+  projectsObj1keyFourArray: [ 'arrayIndex0', 'arrayIndex1' ]
+}, {
+  artistName: 'Metallica',
+  name: 'Metallica',
+  releaseDate: '1991, August 12',
+  genres: [ 'heavy metal' ]
+}, {
+  artistName: 'The Prodigy',
+  name: 'Music for the Jilted Generation',
+  releaseDate: '1994, July 4',
+  genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+}, {
+  artistName: 'Johnny Cash',
+  name: 'Unchained',
+  releaseDate: '1996, November 5',
+  genres: [ 'country', 'rock' ]
+}];
